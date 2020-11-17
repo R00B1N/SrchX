ecaf976cced168367fa667e3905b2dc24c0c2913
Blacksterhack <74608055+Blacksterhack@users.noreply.github.com>
2020-11-17 09:47:41 -0500

    SrchX.py
    Use google dorks to find web page vulnerabilities (passwords, open cameras, user information due to code errors, etc)

---
+++ SrchX.py
@@ -0,0 +1,59 @@
+from googleseacrh import search
+from colorama import Fore, init
+init()
+print(Fore.GREEN)
+
+import time
+time.sleep(1)
+
+banner = """
+000000000000000000000000000
+0   ----  SrchX ----      0
+0           by            0
+0      Blacksterhack      0
+000000000000000000000000000
+"""
+print(banner)
+
+Disclaimer = "This program was created for purposes only"
+print(Disclaimer)
+
+time.sleep(1)
+
+Menu = """
+1- Users and admins passwords(Web pages).
+2- Pages with login forms.
+3- Files with user names or error messages that reveal the username.
+4- Detection of the web server version or vulnerable product.
+5- Hardware devices online(cameras, prints, etc).
+"""
+print(Menu)
+
+#Variables.
+user_admins = "ext:pwd inurl:(service | authors | administrators | users) “# -FrontPage-“"
+L_forms = "inurl:/admin/login.asp"
+f_use_names = "“access denied for user” “using password” “general error” -inurl:phpbb “sql error”"
+dec_web_server = "“SquirrelMail version 1.4.4” inurl:src ext:php"
+hard_dev = "camera linksys inurl:main.cgi"
+
+option = int(input("Choose an option: "))
+
+if option==1:
+	for i in search(user_admins, start = 0, pause = 2):
+		print(i)
+
+if option==2:
+	for i in search(L_forms, start = 0, pause = 2):
+		print(i)
+
+if option==3:
+	for i in search(f_use_names, start = 0, pause = 2):
+		print(i)
+
+if option==4:
+	for i in search(dec_web_server, start = 0, pause = 2):
+		print(i)
+
+if option==5:
+	for i in search(hard_dev, start = 0, pause = 2):
+		print(i)
